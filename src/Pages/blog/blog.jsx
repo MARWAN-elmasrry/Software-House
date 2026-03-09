bc4643b51911407344e2956670bb0a529166a3e8
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2026-03-09 03:25:36 +0200

    blog page done

--- src/Pages/blog/blog.jsx
+++ src/Pages/blog/blog.jsx
@@ -1,6 +1,34 @@
 import { Footer } from "../../components/footer/footer"
 import { Header } from "../../components/header/header"
+import Linesd from "../../assets/linesd.png"
 import "./blog.css"
+
+const projects = [
+    {
+        id: 1,
+        title: "This Main Proj",
+        description: "This Main Proj",
+        image: "https://images.unsplash.com/photo-1486325212027-8081e485255e?w=400&h=250&fit=crop",
+        important: true,
+        link: "#"
+    },
+    {
+        id: 2,
+        title: "This Side Proj",
+        description: "This Main Proj",
+        image: "https://images.unsplash.com/photo-1486325212027-8081e485255e?w=400&h=250&fit=crop",
+        important: false,
+        link: "#"
+    },
+    {
+        id: 3,
+        title: "Another Main Proj",
+        description: "This Main Proj",
+        image: "https://images.unsplash.com/photo-1486325212027-8081e485255e?w=400&h=250&fit=crop",
+        important: true,
+        link: "#"
+    }
+]
 
 export const Blog = ({theme, toggleTheme}) =>{
     return(<>
@@ -14,9 +42,28 @@ export const Blog = ({theme, toggleTheme}) =>{
                             <div className="btn">
                                 <button>CV</button>
                             </div>
+                            <img src={Linesd} />
                         </div>
                         <div className="blog-info">
-
+                            <div className="cards">
+                                {projects.map((project) => (
+                                    <div
+                                        key={project.id}
+                                        className={`card ${project.important ? "card--important" : "card--side"}`}
+                                    >
+                                        <div className="card-inner">
+                                            <div className="card-img">
+                                                <img src={project.image} alt={project.title} />
+                                            </div>
+                                            <div className="card-body">
+                                                <h2 className="card-title">{project.title}</h2>
+                                                <p className="card-desc">{project.description}</p>
+                                                <a href={project.link} className="card-btn">SEE IT</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
