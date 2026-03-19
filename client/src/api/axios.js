5447a27df4efee430bf88d93301f52f10a3faffa
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2026-03-19 03:17:47 +0200

    the Api is Done

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://91.99.172.131:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 
 export default api;
