05e5734e494654709086fdc0f5095b56050ed691
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2026-03-19 03:29:13 +0200

    debug the api in front

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://91.99.172.131:3000/api/",
+  baseURL: "/api/", 
   headers: {
     "Content-Type": "application/json",
   },
