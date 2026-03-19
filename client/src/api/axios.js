d5ecea89d8bfe0f57a408d5c784e2b7a0b9d54fc
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2026-03-19 14:23:09 +0200

    some edits and updates in admin

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
       // "http://91.99.172.131:3000/api/" 
-
-  baseURL: "http://localhost:3000/api/",
+      // http://localhost:3000
+  baseURL: "/api/",
   headers: {
     "Content-Type": "application/json",
   },
