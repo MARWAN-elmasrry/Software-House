4142abf8c2612916c32be12bfae76809d24d4c4f
Marwan Elmasry <141914587+MARWAN-elmasrry@users.noreply.github.com>
2026-03-24 09:53:41 +0200

    Change button text from 'CV' to 'Resume'

--- client/src/Pages/blog/blog.jsx
+++ client/src/Pages/blog/blog.jsx
@@ -35,7 +35,7 @@ export const Blog = ({ theme, toggleTheme }) => {
               <div className="blog-head">
                 <h1>LETS SEE WHERE WE GO TO THE POINT</h1>
                 <div className="btn">
-                  <button>CV</button>
+                  <button>Resume</button>
                 </div>
                 <img src={Linesd} />
               </div>
