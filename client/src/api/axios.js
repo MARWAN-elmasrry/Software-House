8e269bf3d61a515ad2bef9644b664ea75ce1d934
Marwan Elmasry <141914587+MARWAN-elmasrry@users.noreply.github.com>
2026-04-04 20:31:50 +0200

    Clean up axios.js by removing commented URLs
    Removed commented-out base URLs for clarity.

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  // "http://91.99.172.131:3000/api/" 
-  // http://localhost:3000
   baseURL: "/api/",
   headers: {
     "Content-Type": "application/json",
